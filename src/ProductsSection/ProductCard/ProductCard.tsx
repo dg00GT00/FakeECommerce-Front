899f28c216bc776a5325df2711fd47f624b852a0
Gledson <duarte.gledson@gmail.com>
2020-11-30 19:13:56 -0300

    Implemented highlight product card borders

--- src/ProductsSection/ProductCard/ProductCard.tsx
+++ src/ProductsSection/ProductCard/ProductCard.tsx
@@ -28,28 +28,32 @@ export const ProductCard: React.FunctionComponent = () => {
     const cardHeaderStyle = useStyles();
 
     return (
-        <Card className={styles.card}>
-            <CardActionArea className={styles.card_action_area}>
-                <CardHeader title={"Product Title"}
-                            className={[cardHeaderStyle.root, styles.card_title].join(" ")}
-                            action={
-                                <ShareRounded/>
-                            } disableTypography>
-                </CardHeader>
-                <CardMedia className={styles.media} title="Placeholder"/>
-                <CardContent>
-                    <p>
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-                    </p>
-                </CardContent>
-            </CardActionArea>
-            <CardActions className={styles.card_actions}>
-                <div className={styles.price}>$ 100.00</div>
-                <ButtonBase className={styles.cart_arrow_down_button}>
-                    <CartArrowDown className={styles.cart_arrow_down}/>
-                </ButtonBase>
-            </CardActions>
-        </Card>
+        <div className={styles.card_highlight_outer}>
+            <div className={styles.card_highlight_inner}>
+                <Card className={styles.card}>
+                    <CardActionArea className={styles.card_action_area}>
+                        <CardHeader title={"Product Title"}
+                                    className={[cardHeaderStyle.root, styles.card_title].join(" ")}
+                                    action={
+                                        <ShareRounded/>
+                                    } disableTypography>
+                        </CardHeader>
+                        <CardMedia className={styles.media} title="Placeholder"/>
+                        <CardContent>
+                            <p>
+                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                across all continents except Antarctica
+                            </p>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions className={styles.card_actions}>
+                        <div className={styles.price}>$ 100.00</div>
+                        <ButtonBase className={styles.cart_arrow_down_button}>
+                            <CartArrowDown className={styles.cart_arrow_down}/>
+                        </ButtonBase>
+                    </CardActions>
+                </Card>
+            </div>
+        </div>
     );
 }
