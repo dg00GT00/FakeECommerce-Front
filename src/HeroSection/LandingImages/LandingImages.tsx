96806df39837524a4e6b1a724070a1f5e7bec967
Gledson <duarte.gledson@gmail.com>
2020-12-03 10:31:42 -0300

    Adjusted the header (images, fonts and color) to mobile version

--- src/HeroSection/LandingImages/LandingImages.tsx
+++ src/HeroSection/LandingImages/LandingImages.tsx
@@ -2,10 +2,18 @@ import * as React from "react";
 import firstImage from "../../Assets/FrontImages/frontImage1.webp"
 import secondImage from "../../Assets/FrontImages/frontImage2.webp"
 import styles from "./LandingImages.module.scss";
+import {useMediaQuery, useTheme} from "@material-ui/core";
 
 
 export const LandingImages: React.FunctionComponent = props => {
+    const theme = useTheme();
+    const media = useMediaQuery("(max-width: 1210px)")
     const [image, changeImage] = React.useState(firstImage);
+
+    const mediaStyle = media ? {
+        backgroundImage: "none",
+        backgroundColor: theme.palette.primary.dark
+    } : {};
 
     React.useEffect(() => {
         const interval = setInterval(() => {
@@ -20,7 +28,7 @@ export const LandingImages: React.FunctionComponent = props => {
 
     return (
         <div className={styles.front_images}>
-            <div className={styles.gradient}>
+            <div className={styles.gradient} style={mediaStyle}>
                 {props.children}
             </div>
             <img className={styles.images} src={image} alt={"Front page images"} height={"700"}/>
