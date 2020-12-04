e99f8ecebe75576811410edbe237167d1216e3b9
Gledson <duarte.gledson@gmail.com>
2020-12-04 10:46:22 -0300

    Added ability of search bar sticky on its position when scrolling in mobile version

--- src/ProductsSection/ProductNavigation/ProductsNavigationMobile/ProductNavigationMobile.tsx
+++ src/ProductsSection/ProductNavigation/ProductsNavigationMobile/ProductNavigationMobile.tsx
@@ -7,7 +7,7 @@ import MenuItem from "@material-ui/core/MenuItem/MenuItem";
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import styles from "./ProductNavigationMobile.module.scss";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
-import {InputBase, Paper} from "@material-ui/core";
+import {InputBase, Paper, useTheme} from "@material-ui/core";
 
 const seeMoreStyle = makeStyles((theme: Theme) => {
     return createStyles({
@@ -27,6 +27,7 @@ export const ProductNavigationMobile: React.FunctionComponent = () => {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const moreStyle = seeMoreStyle();
     const searchStyle = searchBarStyle();
+    const theme = useTheme();
     const open = Boolean(anchorEl);
 
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -38,31 +39,33 @@ export const ProductNavigationMobile: React.FunctionComponent = () => {
     };
 
     return (
-        <div className={styles.filters_mobile}>
-            <Paper className={[styles.product_search, searchStyle.root].join(" ")}>
-                <IconButton aria-label={"search"}>
-                    <SearchRoundedIcon className={styles.search_icon}/>
+        <div className={styles.mobile_filters_container} style={{backgroundColor: theme.palette.primary.main}}>
+            <div className={styles.mobile_filters}>
+                <Paper className={[styles.product_search, searchStyle.root].join(" ")}>
+                    <IconButton aria-label={"search"}>
+                        <SearchRoundedIcon className={styles.search_icon}/>
+                    </IconButton>
+                    <InputBase placeholder={"Search"} fullWidth className={styles.input_search_bar}/>
+                </Paper>
+                <IconButton
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}>
+                    <MoreVertRoundedIcon className={moreStyle.root}/>
                 </IconButton>
-                <InputBase placeholder={"Search"} fullWidth className={styles.input_search_bar}/>
-            </Paper>
-            <IconButton
-                aria-label="more"
-                aria-controls="long-menu"
-                aria-haspopup="true"
-                onClick={handleClick}>
-                <MoreVertRoundedIcon className={moreStyle.root}/>
-            </IconButton>
-            <Menu
-                id="long-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={open}
-                onClose={handleClose}
-            >
-                <ListSubheader>Filter by</ListSubheader>
-                <MenuItem value={1}>Option 1</MenuItem>
-                <MenuItem value={2}>Option 2</MenuItem>
-            </Menu>
+                <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <ListSubheader>Filter by</ListSubheader>
+                    <MenuItem value={1}>Option 1</MenuItem>
+                    <MenuItem value={2}>Option 2</MenuItem>
+                </Menu>
+            </div>
         </div>
     )
 }
