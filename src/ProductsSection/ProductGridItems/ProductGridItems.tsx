90d4a7376ebde2ac3e41537400ba1e7b518df57f
Gledson <duarte.gledson@gmail.com>
2020-12-11 10:43:09 -0300

    Refactored the product grid items component to receive filter props

--- src/ProductsSection/ProductGridItems/ProductGridItems.tsx
+++ src/ProductsSection/ProductGridItems/ProductGridItems.tsx
@@ -3,20 +3,19 @@ import {ProductCard} from "../ProductCard/ProductCard";
 import {NotFound} from "../../Utilities/RouterValidation/NotFound";
 import {ProductsContext} from "../ProductContext/ProductsContext";
 import {ProductGridSkeleton} from "../ProductGridSkeleton/ProductGridSkeleton";
-import {RouteComponentProps, withRouter} from "react-router-dom";
+import {withRouter} from "react-router-dom";
+import {ProductRouteManagerProps} from "../ProductRouteManager/ProductRouteManagerProps";
 
 type GridItemsType =
     React.FunctionComponentElement<typeof ProductCard>[]
     | React.FunctionComponentElement<typeof NotFound>;
 
-const ProductGrid: React.FunctionComponent<RouteComponentProps & { pageNumber: number }> = ({
-                                                                                                pageNumber,
-                                                                                                location: {pathname},
-                                                                                            }) => {
+const ProductGrid: React.FunctionComponent<ProductRouteManagerProps> = ({location: {pathname}, ...props}) => {
+    // Deconstruct the props in order to prevent infinity request loop
+    const {pageNumber, searchFrag, productBrand, productType, sortFilter} = props;
     const {productReq, setPageCount, setPageNumber, checkHomePageToggle} = React.useContext(ProductsContext);
     const [productGridItems, setProductGrid] = React.useState<GridItemsType | null>(null);
     const [isLoading, toggleLoading] = React.useState(true);
-
 
     React.useEffect(() => {
         if (pathname === "/") {
@@ -25,7 +24,7 @@ const ProductGrid: React.FunctionComponent<RouteComponentProps & { pageNumber: n
             checkHomePageToggle(false);
         }
         productReq
-            .getFullProductList(pageNumber)
+            .getProductList({pageNumber, searchFrag, productBrand, productType, sortFilter})
             .then(productList => {
                 const productItems = productList?.map(product => {
                     return <ProductCard key={product.id} {...product}/>
@@ -39,7 +38,7 @@ const ProductGrid: React.FunctionComponent<RouteComponentProps & { pageNumber: n
                 toggleLoading(false);
                 setProductGrid(<NotFound/>);
             })
-    }, [pathname, productReq, pageNumber, setPageCount, setPageNumber, checkHomePageToggle]);
+    }, [pathname, productReq, pageNumber, searchFrag, productBrand, productType, sortFilter, setPageCount, setPageNumber, checkHomePageToggle]);
 
     return <>{isLoading ? <ProductGridSkeleton skeletonItems={productReq.pageSize}/> : productGridItems}</>
 }
