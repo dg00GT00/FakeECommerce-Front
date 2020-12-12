e63874498399a5d82880b3449a59ed02986a51fe
Gledson <duarte.gledson@gmail.com>
2020-12-12 17:50:01 -0300

    Initialized implementation of auto population of filter product fields by query parameter from url

--- src/ProductsSection/ProductNavigation/ProductsNavigationDesktop/ProductsNavOptions/SortProductOptions.tsx
+++ src/ProductsSection/ProductNavigation/ProductsNavigationDesktop/ProductsNavOptions/SortProductOptions.tsx
@@ -7,6 +7,7 @@ import {ProductSortBy} from "../../../../Utilities/ProductModels/ProductFilters"
 import {useHistory} from "react-router-dom";
 import {ProductFilterEnum, UrlQueryFilter} from "../../../ProductRouteManager/ProductRouteManager";
 import {ProductNavDesktopProps} from "../../ProductNavigationTypes";
+import {parsePath} from "history";
 
 
 const useFormStyles = makeStyles((theme: Theme) =>
@@ -36,29 +37,78 @@ const useFormStyles = makeStyles((theme: Theme) =>
 export const SortProductOptions: React.FunctionComponent<ProductNavDesktopProps> = props => {
     const {className, clearFilter} = props
 
-    const history = useHistory();
     const formStyles = useFormStyles();
+    const {push, location: {search}} = useHistory();
     const [sortedBy, setSort] = React.useState<number | string>("");
+    const [inputValue, setInputValue] = React.useState(-1);
     const formRef = React.useRef<HTMLDivElement>(null);
 
     const handleChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
         setSort(event.target.value as number);
-        history.push({
+        console.log(event);
+        push({
             pathname: '/products',
             search: `${UrlQueryFilter.Sort}=${event.target.value as number}`,
             state: {filter: ProductFilterEnum.FilterSort}
-        })
+        });
     };
-    React.useEffect(() => {
-        setSort("");
-    }, [clearFilter]);
+
+    const clearInput = React.useCallback(() => {
+        let callSubsequentTime = false;
+        return () => {
+            if (callSubsequentTime) {
+                setInputValue(_ => {
+                    const label = formRef.current?.querySelector("label");
+                    if (label) {
+                        label.setAttribute("data-shrink", "true");
+                        label.classList.remove("MuiInputLabel-shrink", "MuiFormLabel-filled")
+                    }
+                    const legend = formRef.current?.querySelector("legend");
+                    if (legend) {
+                        legend.classList.remove("PrivateNotchedOutline-legendNotched-12");
+                    }
+                    return -1;
+                });
+            } else {
+                callSubsequentTime = true;
+            }
+        }
+    }, []);
+
+    const [clearCallback] = React.useState<() => void>(() => clearInput());
 
     React.useEffect(() => {
-        formRef.current?.classList.add(className)
+        if (search.includes(UrlQueryFilter.Sort)) {
+            const searchParams = parsePath(search).search;
+            const queryValue = new URLSearchParams(searchParams)?.get(UrlQueryFilter.Sort);
+            setSort(_ => {
+                const label = formRef.current?.querySelector("label");
+                if (label) {
+                    label.setAttribute("data-shrink", "true");
+                    label.classList.add("MuiInputLabel-shrink", "MuiFormLabel-filled")
+                }
+                const legend = formRef.current?.querySelector("legend");
+                if (legend) {
+                    legend.classList.add("PrivateNotchedOutline-legendNotched-12");
+                }
+                setInputValue(parseInt(queryValue ?? "0"));
+                return queryValue ?? "";
+            });
+        }
+    }, [search])
+
+    React.useEffect(() => {
+        setSort("");
+        clearCallback();
+    }, [clearFilter, clearCallback]);
+
+    React.useEffect(() => {
+        formRef.current?.classList.add(className);
     }, [className])
 
     return (
-        <FormControl size={"small"} fullWidth color={"secondary"} variant="outlined" className={formStyles.root} ref={formRef}>
+        <FormControl size={"small"} fullWidth color={"secondary"} variant="outlined" className={formStyles.root}
+                     ref={formRef}>
             <InputLabel htmlFor="sortIt" id={"sortIt"} classes={{formControl: formStyles.select}}>Sort</InputLabel>
             <Select
                 native
@@ -68,10 +118,10 @@ export const SortProductOptions: React.FunctionComponent<ProductNavDesktopProps>
                 onChange={handleChange}
                 label="Sort"
                 labelId={"sortIt"}
-            >
+                inputProps={{value: inputValue}}>
                 <option/>
-                <option value={ProductSortBy.NameAsc}>Product Name Alphabetically</option>
-                <option value={ProductSortBy.NameDesc}>Product Name Alphabetically Reverse</option>
+                <option value={ProductSortBy.NameAsc}>Name Alphabetically</option>
+                <option value={ProductSortBy.NameDesc}>Name Reverse Alphabetically</option>
                 <option value={ProductSortBy.PriceAsc}>Lower Price</option>
                 <option value={ProductSortBy.PriceDesc}>Higher Price</option>
             </Select>
--- src/ProductsSection/ProductNavigation/ProductsNavigationDesktop/ProductsNavOptions/TypeProductOptions.tsx
+++ src/ProductsSection/ProductNavigation/ProductsNavigationDesktop/ProductsNavOptions/TypeProductOptions.tsx
@@ -7,6 +7,7 @@ import {ProductTypes} from '../../../../Utilities/ProductModels/ProductFilters';
 import {useHistory} from 'react-router-dom';
 import {ProductFilterEnum, UrlQueryFilter} from "../../../ProductRouteManager/ProductRouteManager";
 import {ProductNavDesktopProps} from "../../ProductNavigationTypes";
+import {parsePath} from "history";
 
 
 const useFormStyles = makeStyles((theme: Theme) =>
@@ -36,19 +37,42 @@ const useFormStyles = makeStyles((theme: Theme) =>
 
 export const TypeProductOptions: React.FunctionComponent<ProductNavDesktopProps> = props => {
     const {className, clearFilter} = props;
-    const history = useHistory();
+    const {push, location: {search}} = useHistory();
+    const [inputValue, setInputValue] = React.useState(0)
+
     const formStyles = useFormStyles();
     const formRef = useRef<HTMLDivElement>(null)
     const [type, setType] = React.useState<number | string>("");
 
+
     const handleChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
         setType(event.target.value as number);
-        history.push({
+        push({
             pathname: '/products',
             search: `${UrlQueryFilter.Type}=${event.target.value as number}`,
             state: {filter: ProductFilterEnum.FilterType}
         })
     };
+
+    React.useEffect(() => {
+        if (search.includes(UrlQueryFilter.Type)) {
+            const searchParams = parsePath(search).search;
+            const queryValue = new URLSearchParams(searchParams)?.get(UrlQueryFilter.Type);
+            setType(_ => {
+                const label = formRef.current?.querySelector("label");
+                if (label) {
+                    label.setAttribute("data-shrink", "true");
+                    label.classList.add("MuiInputLabel-shrink", "MuiFormLabel-filled")
+                }
+                const legend = formRef.current?.querySelector("legend");
+                if (legend) {
+                    legend.classList.add("PrivateNotchedOutline-legendNotched-12");
+                }
+                setInputValue(parseInt(queryValue ?? "0"));
+                return queryValue ?? "";
+            });
+        }
+    }, [search])
 
     React.useEffect(() => {
         setType("");
@@ -70,6 +94,7 @@ export const TypeProductOptions: React.FunctionComponent<ProductNavDesktopProps>
                 onChange={handleChange}
                 label="Product Type"
                 labelId={"typeIt"}
+                inputProps={{value: inputValue}}
             >
                 <option/>
                 <option value={ProductTypes.MenClothing}>Men Clothing</option>
