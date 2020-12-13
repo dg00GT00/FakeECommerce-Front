bd15e96869d4f2251ed8864cc17335c8cf152db0
Gledson <duarte.gledson@gmail.com>
2020-12-13 08:45:52 -0300

    Implemented search functionality to product filters

--- src/ProductsSection/ProductNavigation/ProductsNavigationDesktop/ProductsNavOptions/SearchProducts.tsx
+++ src/ProductsSection/ProductNavigation/ProductsNavigationDesktop/ProductsNavOptions/SearchProducts.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import {TextField} from "@material-ui/core";
 import {ProductNavDesktopProps} from "../../ProductNavigationTypes";
+import {ProductFilterEnum, UrlQueryFilter} from "../../../ProductRouteManager/ProductRouteManager";
+import {useHistory} from "react-router-dom";
 
 
 const useFormStyles = makeStyles((theme: Theme) =>
@@ -29,10 +31,31 @@ const useFormStyles = makeStyles((theme: Theme) =>
 
 export const SearchProducts: React.FunctionComponent<ProductNavDesktopProps> = props => {
     const {className, clearFilter} = props;
-
     const formStyles = useFormStyles();
+
+    const {push, replace} = useHistory();
+    const [inputResult, setInputResult] = React.useState("");
     const formRef = React.useRef<HTMLDivElement>(null);
     const inputRef = React.useRef<HTMLInputElement>(null);
+
+    const onInputResult = React.useCallback((): void => {
+        if (inputResult !== "" && inputResult === inputRef.current?.value) {
+            push({
+                pathname: '/products',
+                search: `${UrlQueryFilter.Search}=${inputResult}`,
+                state: {filter: [ProductFilterEnum.FilterSearch]}
+            });
+        }
+        if (inputResult === "" && inputResult === inputRef.current?.value) {
+            replace("/");
+        }
+    }, [inputResult, inputRef, push, replace])
+
+    React.useEffect(() => {
+        const timer = setTimeout(() => onInputResult(), 500);
+        return () => clearTimeout(timer);
+
+    }, [onInputResult]);
 
     React.useEffect(() => {
         if (inputRef.current) {
@@ -51,6 +74,7 @@ export const SearchProducts: React.FunctionComponent<ProductNavDesktopProps> = p
                    type="search"
                    variant="outlined"
                    size={"small"}
+                   onChange={(event => setInputResult(event.target.value))}
                    className={formStyles.root}
                    inputRef={inputRef}
                    ref={formRef}/>
