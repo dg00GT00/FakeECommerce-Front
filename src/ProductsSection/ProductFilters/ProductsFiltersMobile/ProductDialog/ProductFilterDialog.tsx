e78ab09c08bcefbcde00eaef82755a1fea7911b5
Gledson <duarte.gledson@gmail.com>
2020-12-16 21:31:15 -0300

    Added second filter indicator layer

--- src/ProductsSection/ProductFilters/ProductsFiltersMobile/ProductDialog/ProductFilterDialog.tsx
+++ src/ProductsSection/ProductFilters/ProductsFiltersMobile/ProductDialog/ProductFilterDialog.tsx
@@ -5,9 +5,10 @@ import {useProductFilterRoute} from "../../../../Utilities/CustomHooks/useProduc
 import {FilterOptions, ProductFilterState} from "../../../../Utilities/ProductModels/ProductFiltersEnum";
 import {DialogTypesEnum} from './DialogTypesEnum';
 import {FilterOptionsWithIndicator} from "../ProductFilterOptions/FilterOptionsWithIndicator";
+import {ClearFiltersContext} from "../../../../Utilities/ClearFilterManager/ClearFiltersContext";
 
 type ProductDialogProps = {
-    onClose: React.MouseEventHandler<HTMLElement>;
+    onClose: React.MouseEventHandler;
     dialogTitle: DialogTypesEnum
     dialogItems: string[];
     open: boolean;
@@ -15,6 +16,8 @@ type ProductDialogProps = {
 
 export const ProductFilterDialog: React.FunctionComponent<ProductDialogProps> = props => {
     const productFilterRoute = useProductFilterRoute;
+    const {clearSwitch} = React.useContext(ClearFiltersContext);
+    const [indicatorStyle, setIndicatorStyle] = React.useState<{ [i: string]: { backgroundColor: string}; }>({});
     let pushToRoute: (queryValue?: string) => void;
 
     switch (props.dialogTitle) {
@@ -26,13 +29,52 @@ export const ProductFilterDialog: React.FunctionComponent<ProductDialogProps> = 
             break;
     }
 
-    const handleClick: React.MouseEventHandler<HTMLElement> = event => {
+    const onEntering= (node: HTMLElement, isAppearing: boolean) => {
+        node.childNodes.forEach(node => {
+            node.childNodes.forEach(innerNode => {
+                const indicatorEl = innerNode.nextSibling?.childNodes[1];
+                if (indicatorEl) {
+                    const childId = (indicatorEl as HTMLElement).getAttribute("id");
+                    if (childId) {
+                        if (indicatorStyle[childId] && indicatorStyle[childId].backgroundColor !== "initial") {
+                            (indicatorEl as HTMLElement).style.backgroundColor = indicatorStyle[childId].backgroundColor;
+                        }
+                    }
+                }
+            })
+        });
+    }
+
+    const handleClick = (event: React.MouseEvent, index: number) => {
+        const typeId = props.dialogTitle === DialogTypesEnum.ProductTypes ? "dialog_type" : "dialog_brand";
+        const id = `${typeId}_${index}`;
+        setIndicatorStyle(prevIndicator => {
+            const indicator = prevIndicator || indicatorStyle;
+            const indicatorEl = document.getElementById(id);
+            if (indicatorEl) {
+                const backgroundColor = window.getComputedStyle(indicatorEl).backgroundColor;
+                indicator[id] = {...indicator[id], backgroundColor};
+            }
+            for (const indicatorId in indicator) {
+                if (indicatorId !== id) {
+                    indicator[indicatorId] = {...indicator[indicatorId], backgroundColor: "initial"};
+                }
+            }
+            return indicator;
+        })
         pushToRoute(event.currentTarget.textContent?.toLowerCase());
         props.onClose(event);
     }
 
+    React.useEffect(() => {
+        setIndicatorStyle({});
+    }, [clearSwitch])
+
     return (
-        <Dialog onClose={props.onClose} aria-labelledby="simple-dialog-title" open={props.open}>
+        <Dialog onClose={props.onClose}
+                onEntering={onEntering}
+                aria-labelledby="simple-dialog-title"
+                open={props.open}>
             <DialogTitle id="simple-dialog-title">{props.dialogTitle}</DialogTitle>
             <FilterOptionsWithIndicator
                 clickAction={handleClick}
