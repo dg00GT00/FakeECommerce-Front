ec99acbde14495fd60a8d43ed666cb73e67f7c1a
Gledson <duarte.gledson@gmail.com>
2020-12-20 15:08:08 -0300

    Adding validation the sign up input fields

--- src/UserManagerSection/UserSignup/UserSigunp.tsx
+++ src/UserManagerSection/UserSignup/UserSigunp.tsx
@@ -2,12 +2,38 @@ import * as React from "react";
 import {TextField} from "@material-ui/core";
 import {UserInputTypes} from "../../Utilities/ProductModels/UserInputTypes";
 
+type FormId = "username" | "email" | "password" | "repeatPassword";
+
+type FormState = Record<FormId, { errorState: boolean }>;
+
+const initialFormState: FormState = {
+    username: {errorState: false},
+    email: {errorState: false},
+    password: {errorState: false},
+    repeatPassword: {errorState: false},
+};
+
 export const UserSignup: React.FunctionComponent<UserInputTypes> = props => {
-    const {InputLabelProps, InputProps} = props;
+    const [formState, setFormState] = React.useState<FormState>(initialFormState);
+
+    const checkValidity = (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>, formId: FormId) => {
+        setFormState(prevFormState => {
+            const state = prevFormState ?? formState;
+            const errorState = !event.target.checkValidity();
+            return {
+                ...state,
+                [formId]: {
+                    ...state[formId],
+                    errorState
+                }
+            }
+        });
+    }
 
     return (
         <>
             <TextField color={"primary"}
+                       error={formState.username.errorState}
                        fullWidth
                        required
                        id="username"
@@ -15,9 +41,23 @@ export const UserSignup: React.FunctionComponent<UserInputTypes> = props => {
                        type="text"
                        variant="outlined"
                        size={"small"}
-                       InputProps={InputProps}
-                       InputLabelProps={InputLabelProps}
-            />
+                       onBlur={event => checkValidity(event, "username")}
+                       FormHelperTextProps={{error: true}}
+                       helperText={formState.username.errorState ? "the username field is required" : null}
+                       {...props}/>
+            <TextField color={"primary"}
+                       error={formState.email.errorState}
+                       fullWidth
+                       required
+                       id="email"
+                       label="Email"
+                       type="email"
+                       variant="outlined"
+                       size={"small"}
+                       onBlur={event => checkValidity(event, "email")}
+                       FormHelperTextProps={{error: true}}
+                       helperText={formState.email.errorState ? "the email field is required" : null}
+                       {...props}/>
             <TextField color={"primary"}
                        required
                        id="password"
@@ -25,9 +65,8 @@ export const UserSignup: React.FunctionComponent<UserInputTypes> = props => {
                        type="password"
                        variant="outlined"
                        size={"small"}
-                       InputProps={InputProps}
-                       InputLabelProps={InputLabelProps}
-                       fullWidth/>
+                       fullWidth
+                       {...props}/>
             <TextField color={"primary"}
                        id="repeat-password"
                        label="Repeat Password"
@@ -35,9 +74,8 @@ export const UserSignup: React.FunctionComponent<UserInputTypes> = props => {
                        variant="outlined"
                        size={"small"}
                        fullWidth
-                       InputProps={InputProps}
-                       InputLabelProps={InputLabelProps}
-                       required/>
+                       required
+                       {...props}/>
         </>
     );
 }
