3b0a15062776eb112d4d4ed5f6b058562979fdc6
Gledson <duarte.gledson@gmail.com>
2020-12-24 16:47:54 -0300

    Implemented the user login forms

--- src/UserManagerSection/UserLogin/UserLogin.tsx
+++ src/UserManagerSection/UserLogin/UserLogin.tsx
@@ -3,30 +3,48 @@ import {TextField} from "@material-ui/core";
 import {UserInputTypes} from "../../Utilities/ProductModels/UserInputTypes";
 import {NavLink} from "react-router-dom";
 import styles from "./UserLogin.module.scss";
+import {UserFormButton} from "../UserFormButton";
+import {useUserFormValidation} from "../../Utilities/CustomHooks/useUserFormValidation";
 
 
 export const UserLogin: React.FunctionComponent<UserInputTypes> = props => {
+    const {formId, showRequiredLabel, ...inputProps} = props;
+    const {
+        validationFunctions: {emailValidation, passwordValidation, requiredValidation, passwordHelperText},
+        validationState: {errorState, formState, emailState}
+    } = useUserFormValidation(["username", "repeatPassword"], false);
+
     return (
         <>
             <TextField color={"primary"}
+                       error={emailState.email.requiredValidity}
                        fullWidth
-                       id="username"
-                       label="Username"
+                       required
+                       id="email"
+                       label="Email"
                        type="email"
                        variant="outlined"
                        size={"small"}
-                       {...props}
-            />
+                       onBlur={event => emailValidation(event, formState)}
+                       FormHelperTextProps={{error: true}}
+                       helperText={emailState.email.requiredValidity ? "* this field is required" : null}
+                       {...inputProps}/>
             <TextField color={"primary"}
+                       error={formState.password.requiredValidity || formState.password.patternValidity}
+                       required
                        id="password"
                        label="Password"
                        type="password"
                        variant="outlined"
                        size={"small"}
                        fullWidth
-                       {...props}
-            />
+                       onChange={passwordValidation}
+                       onBlur={event => requiredValidation(event, "password")}
+                       FormHelperTextProps={{error: true}}
+                       helperText={passwordHelperText(formState)}
+                       {...inputProps}/>
             <NavLink to={"/user/signup"} className={styles.forgot_password}>Have no account?</NavLink>
+            <UserFormButton formId={formId} formValidity={errorState} formState={formState}/>
         </>
     )
 }
