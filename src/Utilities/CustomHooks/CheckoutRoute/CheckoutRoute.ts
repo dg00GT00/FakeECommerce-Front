c9a19e13c42752dcfb564c56679ba7e20d4bd76a
Gledson Duarte <duarte.gledson@gmail.com>
2021-01-17 16:51:31 -0300

    Update the checkout route hook to use login state

---
+++ src/Utilities/CustomHooks/CheckoutRoute/CheckoutRoute.ts
@@ -0,0 +1,3 @@
+export enum CheckoutRoute {
+    TO_LOGIN
+}
--- src/Utilities/CustomHooks/CheckoutRoute/useCheckoutRoute.tsx
+++ src/Utilities/CustomHooks/CheckoutRoute/useCheckoutRoute.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {useContext} from "react";
 import {useHistory} from "react-router-dom";
 import {AuthContext} from "../../Context/AuthContext";
+import { CheckoutRoute } from "./CheckoutRoute";
 
 export const useCheckoutRoute = () => {
     const {push} = useHistory();
@@ -10,7 +11,8 @@ export const useCheckoutRoute = () => {
     return (event: React.MouseEvent) => {
         if (!getJwt()) {
             push({
-                pathname: "/user/login"
+                pathname: "/user/login",
+                state: CheckoutRoute.TO_LOGIN
             });
             return;
         }
